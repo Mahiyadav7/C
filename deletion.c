9731868ecb033f14f01015b2f826947fc0e2c24c
Mahi <mahi004tb@gmail.com>
2025-02-06 22:51:10 +0530

    Add files via upload
    Deleting element from an array

---
+++ deletion.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+/*int inputArray(int n){
+    int i;
+    printf("enter the size of array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter elements in array:\n");
+    for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    }
+    return arr;
+
+}*/
+
+void deleteElement(int arr[],int pos,int n ,int x){
+int arr2[n-1];
+int i;
+         for(i=pos;i<n-1;i++){
+          arr2[i]=arr[i+1];
+         }
+         for(i=0;i<pos;i++){
+            
+            arr2[i]=arr[i];
+            
+            
+         }
+         printf("array after deletion:\n");
+         for(i=0;i<n-1;i++){
+            printf("%d  ",arr2[i]);
+         }
+}
+
+int main(){
+    int n,i;
+    printf("enter the size of array:");
+    scanf("%d",&n);
+  
+    int arr[n];
+    printf("enter elements in array:\n");
+    for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    }
+    printf("original  array:\n");
+    for(i=0;i<n;i++){
+   printf("%d ",arr[i]);
+    }
+int x,pos;
+   printf("enter the value and postion of the element you would like to delete respectively:\n");
+   scanf("%d",&x);
+    scanf("%d",&pos);
+  deleteElement(arr,pos,n,x);
+return 0;
+}
